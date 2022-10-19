9cda20c302969a387434d9caf094d8ad0bcf709e
marysonthego <marysonthego@gmail.com>
2022-10-19 16:08:26 -0600

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,88 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let anything: any;  // any type
+  let thing: unknown;  // unknown type use instead of any type
+  let name: string;
+  let age: number | string; // union type age can be a number or a string
+
+  age = 25; // age can be a number or a string
+  age = '25';
+
+  let print: (name: string) => never; // a function never returns nothing
+
+  //define the printName function
+  function printName(fn: (name: string) => void) { // a function void returns undefined
+    fn("John was here"); //call the function sent as a argument (printToConsole)
+  }
+
+  //define the function printToConsole
+  function printToConsole(s: string) {
+    console.log(s);
+  }
+
+  //call the function printName. It takes a function with name: string arguments */}
+  printName(printToConsole);
+
+  let isStudent: boolean;
+  let hobbies: string[];
+  let role: [number, string]; // Tuple
+
+  role = [2, 'author'];
+
+  type Person = {
+    name: string;
+    age?: number; // ? = Optional
+  };
+
+  type XXX = {
+    a: string;
+    b: number;
+  }
+
+  interface APerson extends XXX {  //interface Person extends type XXX
+    name: string;
+    age?: number; // Optional
+  }
+
+  type XX = Person & { //intersection of type XX with interface Person
+    a: string;
+    b: string;
+  }
+
+  interface Guy extends Person { //interface Guy extends interface Person
+    profession: string;
+  }
+
+  type X = {
+    a: string;
+    b: number;
+  };
+
+  type Y = X & { // Intersection Y type contains all the properties of X and the properties of Y
+    c: string;
+    d: number;
+  }
+
+  let y: Y = {
+    a: 'a',
+    b: 1,
+    c: 'c',
+    d: 2
+  }
+
+  let person: Person = {
+    name: 'John',
+  }
+
+  let lotsOfPeople:Person[]; // Array of Person objects
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Hello World!</p>
+
     </div>
   );
 }
