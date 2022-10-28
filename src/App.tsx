86d25fd383cdd86e8a184c1fda84a064c7465664
marysonthego <marysonthego@gmail.com>
2022-10-28 16:20:13 -0600

    context API

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,58 @@
-import React, {useState} from 'react';
-import './App.css';
-import {InputField} from './components/InputField';
-import {TodoList} from './components/TodoList';
-import {Todo} from './components/model';
-import {Person, PersonDestructured, PersonFC, HairColor} from './components/Person';
-import {DragDropContext, DropResult} from 'react-beautiful-dnd';
+import React, { createContext, useState } from "react";
+import "./App.css";
+import { InputField } from "./components/InputField";
+import { TodoList } from "./components/TodoList";
+import { Todo } from "./components/model";
+import {
+  Person,
+  PersonDestructured,
+  PersonFC,
+  HairColor,
+} from "./components/Person";
+import { DragDropContext, DropResult } from "react-beautiful-dnd";
 
-const  App: React.FC = () => {
+interface AppContextInterface {
+  name: string;
+  age: number;
+  country: string;
+}
 
-  const [todo, setTodo] = useState<string | number>('');
-  const [todos, setTodos] = useState<Todo[]> ([]);
+const AppContext = createContext<AppContextInterface | null > (null);
+
+const App: React.FC = () => {
+  const contextValue = {
+    name: "Jack",
+    age: 20,
+    country: "Brazil",
+  };
+
+  const [todo, setTodo] = useState<string | number>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
   const [completedTodos, setCompletedTodos] = useState<Todo[]>([]);
 
   const handleAdd = (e: React.FormEvent) => {
     e.preventDefault();
-    if(todo) {
-      setTodos([...todos, {id: Date.now(), todo, isDone: false}]);
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
       setTodo("");
     }
-  }
+  };
 
-  const onDragEnd=(result: DropResult) => {
-    const {source, destination} = result;
+  const onDragEnd = (result: DropResult) => {
+    const { source, destination } = result;
     console.log(result);
     if (!destination) return;
-    if (destination.droppableId === source.droppableId &&
-        destination.index === source.index)
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    )
       return;
 
     let add,
-        active = todos,
-        complete = completedTodos;
+      active = todos,
+      complete = completedTodos;
 
-    if (source.droppableId === 'TodoList') {
+    if (source.droppableId === "TodoList") {
       add = active[source.index];
       active.splice(source.index, 1);
     } else {
@@ -40,7 +60,7 @@ const  App: React.FC = () => {
       complete.splice(source.index, 1);
     }
 
-    if (destination.droppableId === 'TodoList') {
+    if (destination.droppableId === "TodoList") {
       active.splice(destination.index, 0, add);
     } else {
       complete.splice(destination.index, 0, add);
@@ -50,31 +70,46 @@ const  App: React.FC = () => {
     setTodos(active);
   };
 
-
   return (
-    <DragDropContext onDragEnd={onDragEnd}>
-    <div className="App">
-      <span className="heading">
-        Taskify
-      </span>
-      <span className="heading"> Person:
-        <Person name="Jack" email="marysonthego@gmail.com" hairColor={HairColor.Blonde} />
-      </span>
-      <span className="heading">PersonDestructured:
-        <PersonDestructured name="Jack" email="marysonthego@gmail.com" hairColor={HairColor.Blonde}/>
-      </span>
-      <span className="heading">PersonFC:
-        <PersonFC name="Pedro" email="marysonthego@gmail.com" hairColor={HairColor.Blonde}/>
-      </span>
-      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
-      <TodoList
-        todos={todos}
-        setTodos={setTodos}
-        completedTodos = {completedTodos}
-        setCompletedTodos = {setCompletedTodos}
-      />
-    </div>
-    </DragDropContext>
+    <AppContext.Provider value={contextValue}>
+      <DragDropContext onDragEnd={onDragEnd}>
+        <div className="App">
+          <span className="heading">Taskify</span>
+          <span className="heading">
+            {" "}
+            Person:
+            <Person
+              name="Jack"
+              email="marysonthego@gmail.com"
+              hairColor={HairColor.Blonde}
+            />
+          </span>
+          <span className="heading">
+            PersonDestructured:
+            <PersonDestructured
+              name="Jack"
+              email="marysonthego@gmail.com"
+              hairColor={HairColor.Blonde}
+            />
+          </span>
+          <span className="heading">
+            PersonFC:
+            <PersonFC
+              name="Pedro"
+              email="marysonthego@gmail.com"
+              hairColor={HairColor.Blonde}
+            />
+          </span>
+          <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+          <TodoList
+            todos={todos}
+            setTodos={setTodos}
+            completedTodos={completedTodos}
+            setCompletedTodos={setCompletedTodos}
+          />
+        </div>
+      </DragDropContext>
+    </AppContext.Provider>
   );
 };
 
